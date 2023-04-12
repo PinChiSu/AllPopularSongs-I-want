484bd31a9b579f90867bf9a09f087084971c81cc
PinChiSu <53268937+PinChiSu@users.noreply.github.com>
2023-04-12 23:02:06 +0200

    remove stray prints

--- get_data.py
+++ get_data.py
@@ -179,15 +179,10 @@ def main():
     args = parse_args()
     client_id, secret, keyword = args.id, args.s, args.k
     spotify_client = SpotifyClient(client_id, secret, 'http://localhost:3000/')
-    print(HOST)
     database = Database(HOST, DATABASE_NAME, DATABASE_USER, DATABASE_PASSWORD, 'keyword_tracks')
 
     # Create the 'tracks' table if it doesn't exist yet
     database.create_tracks_table()
-    print("In Python")
-    print(client_id)
-    print(secret)
-    print(keyword)
     # Import the playlist into the database
     playlist_importer = PlaylistImporter(
         keyword,  # get the keyword
